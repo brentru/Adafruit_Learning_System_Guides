3b77841b716575dbe0bf4ea5edd33cf7c49722d7
brentru <robots199@me.com>
2020-12-30 16:56:27 -0500

    move steps to high-level helper methods...

--- PyPortal_Google_Calendar/code.py
+++ PyPortal_Google_Calendar/code.py
@@ -5,6 +5,10 @@ from digitalio import DigitalInOut
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
 from adafruit_esp32spi import adafruit_esp32spi
 import adafruit_requests as requests
+import displayio
+import terminalio
+from adafruit_display_text.label import Label
+
 
 # Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
 # "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
@@ -42,18 +46,27 @@ print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
 socket.set_interface(esp)
 requests.set_socket(socket, esp)
 
+# DisplayIO
+
+# Group for displaying verification code and URL
+group_verification = displayio.Group(max_size=25)
+label_verification_url = Label(terminalio.FONT, x=0, y=10, max_glyphs=40)
+label_user_code = Label(terminalio.FONT, x=0, y=40, max_glyphs=15)
+group_verification.append(label_verification_url)
+group_verification.append(label_user_code)
+
 ### helper methods ###
 def display_user_code(user_code, verification_url):
     """Displays the verificaiton URL and user code to the user.
     "param str user_code: identifies scopes requested by the application
     :param str verification_url: url user must navigate to on a browser.
     """
-    # display the user code
-    # NOTE: user_code must be displayed with a length field of 15 wide
-
-    # display the verification url
-    # NOTE: Length must be 40 characters long
-    pass
+    # 320 x 240
+    # set labels
+    label_user_code.text = user_code
+    label_verification_url = verification_url
+    # show the group
+    board.DISPLAY.show(group_verification)
 
 # Poll Google's authorization server
 def poll_google_auth_server(interval_time, expiration_time):
@@ -75,22 +88,26 @@ def poll_google_auth_server(interval_time, expiration_time):
 
 # The following steps are used for obtaining OAuth 2.0 access tokens
 # https://developers.google.com/identity/protocols/oauth2/limited-input-device
+def request_device_user_codes():
+    """Sends a HTTP POST request to Google's authorization server
+    that identifies your application and the access scope.
 
+    Returns response object.
+    """
+    URL = "https://oauth2.googleapis.com/device/code?client_id={0}&scope=https://www.googleapis.com/auth/calendar.readonly".format(secrets['google_client_id'])
+    HEADERS = {"Host": "oauth2.googleapis.com",
+               "Content-Type": "application/x-www-form-urlencoded",
+               "Content-Length":"0"}
+    # TODO: May need to add some handling code if user provides invalid ID
+    response = requests.post(URL, headers=HEADERS)
+    return response
 
-
-# Request device and user codes
-print("POSTing data")
-URL = "https://oauth2.googleapis.com/device/code?client_id={0}&scope=https://www.googleapis.com/auth/calendar.readonly".format(secrets['google_client_id'])
-HEADERS = {"Host": "oauth2.googleapis.com",
-                     "Content-Type": "application/x-www-form-urlencoded",
-                     "Content-Length":"0"}
-response = requests.post(URL, headers=HEADERS)
-print("-" * 40)
-
-# TODO: add some handling, if we cant parse or response back is invalid
-json_resp = response.json()
-print(json_resp)
-
+### User-Code ###
+print("Requesting device and user codes...")
+resp = request_device_user_codes()
+# Convert to JSON
+json_resp = resp.json()
+# Parse out what we need from the response...
 # unique device identifier
 device_code = json_resp['device_code']
 # length of time, in seconds that the codes above are valid
@@ -102,4 +119,13 @@ verification_url = json_resp['verification_url']
 # identifies scopes requested by the application
 user_code = json_resp['user_code']
 
-display_user_code()
+print("Displaying user code and verification URL...")
+display_user_code(user_code, verification_url)
+
+# TODO
+print("Polling google's auth server...")
+# poll_google_auth_server(interval_time, expiration_time)
+
+# prevent exit
+while True:
+    pass
