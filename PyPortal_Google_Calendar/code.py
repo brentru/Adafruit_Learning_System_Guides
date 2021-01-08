c19141384912ebdf19fcd15a1950d36a4184618e
brentru <robots199@me.com>
2021-01-08 11:41:40 -0500

    start adding ui

--- PyPortal_Google_Calendar/code.py
+++ PyPortal_Google_Calendar/code.py
@@ -10,6 +10,8 @@ import adafruit_requests as requests
 from adafruit_oauth2 import OAuth2
 import displayio
 from adafruit_display_shapes.line import Line
+from adafruit_bitmap_font import bitmap_font
+from adafruit_display_text import label
 from adafruit_pyportal import PyPortal
 import rtc
 
@@ -21,6 +23,11 @@ MAX_EVENTS = 3
 
 # Amount of time to wait between refreshing the calendar, in minutes
 REFRESH_TIME = 15
+
+MONTHS = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr',
+          5:'May', 6:'Jun', 7:'Jul', 8:'Aug',
+          9:'Sep', 10:'Oct', 11:'Nov',
+          12:'Dec'}
 
 # Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
 # "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
@@ -66,37 +73,6 @@ google_auth = OAuth2(
     secrets["google_refresh_token"]
 )
 
-# Initial refresh of access token
-print("Refreshing access token..")
-# TODO: Take a timestamp of when we requested this
-# so we can check against expiration!
-if not google_auth.refresh_access_token():
-    raise RuntimeError("Unable to refresh access token - has the token been revoked?")
-
-# DisplayIO
-frame = displayio.Group(max_size=15)
-
-# Add a white background
-background = displayio.Group(max_size=1)
-color_bitmap = displayio.Bitmap(
-    board.DISPLAY.width, board.DISPLAY.height, 1
-)
-color_palette = displayio.Palette(1)
-color_palette[0] = 0xFFFFFF
-bg_sprite = displayio.TileGrid(
-        color_bitmap,
-        pixel_shader=color_palette,
-        x=0, y=0)
-background.append(bg_sprite)
-frame.append(background)
-
-# Add the header
-line_header = Line(0, 60, 320, 60, color=0x000000)
-frame.append(line_header)
-# TODO: Add font to header
-
-board.DISPLAY.show(frame)
-
 def get_current_time():
     """Gets local time from Adafruit IO and converts to RFC3339 timestamp.
 
@@ -131,7 +107,7 @@ def get_calendar_events(calendar_id, max_events, time_min):
         events.append(event_items[event])
     return events
 
-def format_datetime(datetime):
+def format_datetime(datetime, pretty_date=False):
     """Formats ISO-formatted datetime returned by Google Calendar API into
     a struct_time.
     :param str datetime: Datetime string returned by Google Calendar API
@@ -143,6 +119,8 @@ def format_datetime(datetime):
     the_time = times[1]
     year, month, mday = [int(x) for x in the_date.split("-")]
     the_time = the_time.split("-")[0]
+    if "Z" in the_time:
+        the_time = the_time.split("Z")[0]
     hours, minutes, seconds = [int(x) for x in the_time.split(":")]
     am_pm = "am"
     if hours >= 12:
@@ -157,6 +135,9 @@ def format_datetime(datetime):
         # event is another day, return the full datetime
         formatted_date = '{:02d}/{:02d}/{:04d} '.format(month, mday, year)
         return formatted_date + formatted_time
+    elif pretty_date: # return a nice date for header label
+        formatted_date = '{}. {:02d}, {:04d} '.format(MONTHS[month], mday, year)
+        return formatted_date
     # Event occurs today, return the time only
     return formatted_time
 
@@ -179,16 +160,39 @@ def display_calendar_events(events):
         # Generate new label to hold event info
         # TODO
 
+# Initial refresh of access token
+print("Refreshing access token..")
+# TODO: Take a timestamp of when we requested this
+# so we can check against expiration!
+if not google_auth.refresh_access_token():
+    raise RuntimeError("Unable to refresh access token - has the token been revoked?")
+
+
+pyportal.set_background(0xFFFFFF)
+
+# Add the header
+line_header = Line(0, 60, 320, 60, color=0x000000)
+pyportal.splash.append(line_header)
+font_h1 = bitmap_font.load_font("fonts/Arial-Bold-24.bdf")
+label_header = label.Label(font_h1, x=0, y=40,
+                           color=0x000000, max_glyphs=50)
+pyportal.splash.append(label_header)
+
+
 
 while True:
     # fetch calendar events!
     print("fetching local time...")
     now = get_current_time()
+    label_header.text = format_datetime(now, pretty_date=True)
+
     print("fetching calendar events...")
     events = get_calendar_events(CALENDAR_ID, MAX_EVENTS, now)
 
     print("displaying events")
     display_calendar_events(events)
 
-    print("Sleeping for %d minutes"%(REFRESH_TIME*60))
+    board.DISPLAY.show(pyportal.splash)
+
+    print("Sleeping for %d minutes"%REFRESH_TIME)
     time.sleep(REFRESH_TIME * 60)
